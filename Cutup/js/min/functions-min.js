3693ba840b90dd7b1a1366597911388df3d856eb
Tom Leenders <leenders.thomas@gmail.com>
2016-10-25 17:16:34 +0800

    clean out old JS
    js carnage

--- Cutup/js/min/functions-min.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".hamburger").click(function(){return $(".mob-menu").css({height:$(document).height()}),$("body").hasClass("is-mobOpen")?$("body").removeClass("is-mobOpen"):$("body").addClass("is-mobOpen"),!1})});
