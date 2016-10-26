67d2bf4d5e5481c2f536c5ee81d09933c45ec1ba
Tom Leenders <leenders.thomas@gmail.com>
2016-10-26 16:55:00 +0800

    fix deployment script so it doesn’t include some files
    a quick hack so we don’t get node_modules being uploaded

--- Cutup/bin/deploy.sh
+++ Cutup/bin/deploy.sh
@@ -2,4 +2,4 @@
 
 APP_PATH=$(pwd) ./node_modules/gulp/bin/gulp.js &&
 NODE_ENV=production node ./node_modules/webpack/bin/webpack.js --p --progress &&
-rsync -av ../Cutup/ fenders@fende.rs:~/public_html/
+rsync -av --exclude 'node_modules' --exclude 'webpack.*' --exclude 'gulpfile.js' --exclude 'bin' ../Cutup/ fenders@fende.rs:~/public_html/
