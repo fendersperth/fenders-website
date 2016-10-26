0325db5f889fabae3b1cd2961956f291c11dd6de
Lee Karolczak <lee@humaan.com.au>
2016-10-26 09:26:55 +0800

    Update footer and add hard-coded events.

--- Cutup/parts/footer.php
+++ Cutup/parts/footer.php
@@ -1,20 +1,20 @@
 <footer class="clearfix" role="contentinfo">
     <div class="container">
 
-        <a class="logo" href="" title="Fenders">Fenders</a>
+        <a class="logo" href="" title="Fenders"><span>Fenders</span></a>
 
-        <nav>
+        <?php /* <nav>
             <ul>
                 <li><a href="" title="Submit an event">Submit an event</a></li>
                 <li><a href="" title="Sponsorship">Sponsorship</a></li>
                 <li><a href="" title="Email us">Email us</a></li>
             </ul>
-        </nav>
+        </nav> */ ?>
 
         <nav class="social">
             <ul>
-                <li><a class="twitter" href="" title="Twitter">Twitter</a></li>
-                <li><a class="slack" href="" title="Slack">Slack</a></li>
+                <li><a class="twitter" href="https://twitter.com/fendersperth" title="Twitter"><span>Twitter</span></a></li>
+                <li><a class="slack" href="https://fendersslack.herokuapp.com/" title="Slack"><span>Slack</span></a></li>
             </ul>
         </nav>
 
