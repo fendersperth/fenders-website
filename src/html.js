d4cff4508e3b53d755cebebdfc42c88f245cf021
Jason O'Neil <jason.oneil@gmail.com>
2018-01-20 19:58:27 +0800

    Properly ignore public/* as recommended by Gatsby
    As a result of this, I've moved all assets from public/static
    to src/assets, and they are now loaded via webpack.
    
    There's some weirdness with loading fender-styles.css, it's currently being
    imported in html.js, pages/index.js, as well as via `public/styles.css` when
    building for production. It's a bit weird, but it works, and I figure we're
    switching to glamorous at some point so I'm not too worried for now.

--- src/html.js
+++ src/html.js
@@ -1,24 +1,25 @@
 import React from "react"
+import './assets/css/fenders-styles.css'
 
-let stylesStr
+let stylesStr;
 if (process.env.NODE_ENV === `production`) {
   try {
-    stylesStr = require(`!raw-loader!../public/styles.css`)
+    stylesStr = require(`!raw-loader!../public/styles.css`);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
 module.exports = class HTML extends React.Component {
   render() {
-    let css
+    let css;
     if (process.env.NODE_ENV === `production`) {
       css = (
         <style
           id="gatsby-inlined-css"
           dangerouslySetInnerHTML={{ __html: stylesStr }}
         />
-      )
+      );
     }
     return (
         <html {...this.props.htmlAttributes} className="home">
@@ -30,18 +31,15 @@ module.exports = class HTML extends React.Component {
                 content="width=device-width, initial-scale=1, shrink-to-fit=no"
                 />
                 {this.props.headComponents}
-
-                <link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32" />
-                <link rel="icon" type="image/png" href="img/favicon-16x16.png" sizes="16x16" />
-                <link rel="manifest" href="img/manifest.json" />
-                <link rel="mask-icon" href="img/safari-pinned-tab.svg" color="#614b87"/>
-                <meta name="theme-color" content="#614b87" />
-                <link rel="stylesheet" href="./static/css/fenders-styles.css" media="screen" />
-                <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
-
                 {css}
 
+                <link rel="apple-touch-icon" sizes="120x120" href={require('./assets/img/apple-touch-icon.png')} />
+                <link rel="icon" type="image/png" href={require('./assets/img/favicon-32x32.png')} sizes="32x32" />
+                <link rel="icon" type="image/png" href={require('./assets/img/favicon-16x16.png')} sizes="16x16" />
+                <link rel="manifest" href={require('./assets/img/manifest.json')} />
+                <link rel="mask-icon" href={require('./assets/img/safari-pinned-tab.svg')} color="#614b87"/>
+                <meta name="theme-color" content="#614b87" />
+                <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
             </head>
             <body {...this.props.bodyAttributes}>
                 {this.props.preBodyComponents}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
 import { Events } from '../components/Events'
+import '../assets/css/fenders-styles.css'
 
 export default () => (
         <div className="page-container">
