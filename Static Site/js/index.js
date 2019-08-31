b3cddc6a29b73f3cea72de9e5b2478b5430132e8
Anton Ball <antonball@gmail.com>
2019-08-31 21:30:28 +0800

    Removes unused code/old sites/deps

--- Static Site/js/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form';
-
-ReactDOM.render(
-	<Form />,
-	document.getElementById('join-form')
-);
--- bin/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-APP_PATH=$(pwd) ./node_modules/gulp/bin/gulp.js &&
-NODE_ENV=production node ./node_modules/webpack/bin/webpack.js --p --progress &&
-rsync -av --exclude 'node_modules' --exclude 'webpack.*' --exclude 'gulpfile.js' --exclude 'bin' ../Cutup/ fenders@fende.rs:~/public_html/
