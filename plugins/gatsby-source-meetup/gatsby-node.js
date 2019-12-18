3103edde6aa22e7d1a575c49be83ca820756512d
Anton <antonball@gmail.com>
2019-12-19 05:50:04 +0800

    Fixes Gatsby Source Meetup plugin (#37)

--- plugins/gatsby-source-meetup/gatsby-node.js
+++ plugins/gatsby-source-meetup/gatsby-node.js
@@ -1,8 +1,27 @@
-const crypto = require('crypto')
 const fetch = require('node-fetch')
 
-exports.sourceNodes = async ({ actions, createNodeId }, { apiURL }) => {
+exports.createSchemaCustomization = ({ actions }) => {
+    const { createTypes } = actions
+
+    createTypes(`
+        type Event implements Node {
+            id: String
+            name: String
+            link: String
+            local_date: Date @dateformat
+            }
+    `)
+}
+
+exports.sourceNodes = async (
+    { actions, createNodeId, createContentDigest, reporter },
+    { apiURL }
+) => {
     const { createNode } = actions
+
+    if (!apiURL) {
+        reporter.panic('Please define an apiURL param to gatsby-source-meetup')
+    }
 
     try {
         const res = await fetch(apiURL)
@@ -15,17 +34,13 @@ exports.sourceNodes = async ({ actions, createNodeId }, { apiURL }) => {
                 parent: null,
                 children: [],
                 internal: {
-                    type: 'event',
+                    type: 'Event',
                     content: JSON.stringify(event),
-                    contentDigest: crypto
-                        .createHash('md5')
-                        .update(JSON.stringify(event))
-                        .digest('hex'),
-                    description: 'Meetup event',
+                    contentDigest: createContentDigest(event),
                 },
             })
         })
     } catch (err) {
-        throw new Error('Error creating Meetup events.')
+        reporter.panicOnBuild('Error creating Meetup events.')
     }
 }
